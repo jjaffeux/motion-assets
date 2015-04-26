a32fdc6af5ec33e4926e6c60702aaed3bff13489
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2015-04-26 23:59:45 +0200

    update ios assets list

--- lib/motion/project/assets.rb
+++ lib/motion/project/assets.rb
@@ -19,32 +19,35 @@ module Motion::Project
     BASE_ANDROID_ICON_SIZE = [512, 512]
 
     IOS_ICONS = %w{
-      Icon-Small.png|29x29
-      Icon-Small@2x.png|58x58
-      Icon-Small@3x.png|87x87
-      Icon-40.png|40x40
-      Icon-40@2x.png|80x80
-      Icon-40@3x.png|120x120
-      Icon-60@2x.png|120x120
-      Icon-60@3x.png|180x180
-      Icon-76.png|76x76
-      Icon-76@2x.png|152x152
-      Icon-120.png|120x120
-      iTunesArtwork.png|512x512
-      iTunesArtwork@2x.png|1024x1024
+      icon.png|57x57
+      icon@2x.png|114x114
+      icon-40.png|40x40
+      icon-40@2x.png|80x80
+      icon-50.png|50x50
+      icon-50@2x.png|100x100
+      icon-60.png|60x60
+      icon-60@2x.png|120x120
+      icon-60@3x.png|180x180
+      icon-72.png|72x72
+      icon-72@2x.png|144x144
+      icon-76.png|76x76
+      icon-76@2x.png|152x152
+      icon-small.png|29x29
+      icon-small@2x.png|58x58
+      icon-small@3x.png|87x8
     }
 
     IOS_SPLASHES = %w{
-      Default~iphone.png|320x480
-      Default@2x~iphone.png|640x960
-      Default-Portrait~ipad.png|768x1024
-      Default-Portrait@2x~ipad.png|1536x2048
-      Default-Landscape~ipad.png|1024x768
-      Default-Landscape@2x~ipad.png|2048x1536
       Default-568h@2x~iphone.png|640x1136
-      Default-667h.png|375x667
+      Default-667h.png|750x1334
       Default-736h.png|1242x2208
       Default-Landscape-736h.png|2208x1242
+      Default-Landscape@2x~ipad.png|2048x1536
+      Default-Landscape~ipad.png|1024x768
+      Default-Portrait@2x~ipad.png|1536x2048
+      Default-Portrait~ipad.png|768x1024
+      Default@2x~iphone.png|640x960
+      Default~iphone.png|320x48
     }
 
     ANDROID_ICONS = %w{
